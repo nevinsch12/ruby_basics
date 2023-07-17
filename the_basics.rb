3a022d986333fd9a474d962711a3867be36bdffc
nevinsch12 <christopher.nevins@icloud.com>
2023-07-16 19:07:38 -0500

    The Basics

---
+++ the_basics.rb
@@ -0,0 +1,32 @@
+# The Basics
+
+# 1.
+"Christopher " + "Nevins"
+
+# 2.
+thousands = 7843 / 1000
+hundreds = 7843 / 100 % 10
+tens = 7843 / 10 % 10
+ones = 7843 % 10
+
+# 3. 
+movies = {"Monty Python": 1975, "The Day After Tomorrow": 2004, "Star Trek": 2013, "A Beautiful Mind": 2001, "Stripes": 1981}
+puts movies[:"Monty Python"]
+puts movies[:"The Day After Tomorrow"]
+puts movies[:"Star Trek"]
+puts movies[:"A Beautiful Mind"]
+puts movies[:"Stripes"]
+
+#4. 
+movies = [1975, 2004, 2013, 2001, 1981]
+puts movies
+
+#5. 
+puts 5 * 6 * 7 * 8
+
+#6.
+puts 8.0 ** 2
+puts 7.353 ** 2
+puts 9.0283 ** 2
+
+#7. This tells me that there is a missing closing bracket.
